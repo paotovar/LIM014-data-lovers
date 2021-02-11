12443cdc78756ba329e2fd8ec64e6bbfe392af8d
paotovar <ptovargavilan@gmail.com>
2021-02-11 12:01:40 -0500

    Cambios en el index

--- src/data.js
+++ src/data.js
@@ -1,9 +1,33 @@
 // estas funciones son de ejemplo
-
+/*
 export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
+};
+*/
+export const orderData = (data, sortBy, sortOrder) => {
+  let result;
+  if (sortOrder === 'ascendente') {
+    result = data.sort((a, b) => {
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+      return -1;
+    });
+  } else {
+    result = data.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return 1;
+      }
+      return -1;
+    });
+  }
+  return result;
+};
+export const searchPokemons = (data, searchBy, condition) => {
+  const search = data.filter(letter => (letter[searchBy]).toLowerCase().indexOf(condition) !== -1);
+  return search;
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+/*import { example } from './data.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';*/
+
+
+import data from './data/pokemon/pokemon.js';
+
+import {
+  orderData,
+  searchPokemons,
+} from './data.js';
+
+/*console.log(example, data);*/
+
+const firstScreen = document.getElementById('firstScreen');
+const secondScreen = document.getElementById('secondScreen');
+
+//Boton para mostrar pokemones
+const btnMostrar = document.getElementById('btnMostrar');
+btnMostrar.addEventListener('click', () => {
+  firstScreen.classList.add('hide');
+  secondScreen.classList.remove('hide');
+});
+
+
+const pokemonList = data.pokemon;
+const contentList = document.querySelector('#contentList');
+
+pokemonList.forEach(pokemon=>{
+    return contentList.innerHTML+=`<h2>${pokemon.name}</h2>`
+})
+
+
